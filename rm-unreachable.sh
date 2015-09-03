b80b3abc40d459f5c354002259234bd47ced8be3
0x90 <oleg.kupreev@gmail.com>
2015-09-03 04:21:20 +0300

    rm-unreachable.sh

--- rm-unreachable.sh
+++ rm-unreachable.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# http://stackoverflow.com/questions/3765234/listing-and-deleting-git-commits-that-are-under-no-branch-dangling
 
 git reflog expire --expire-unreachable=now --all
 git gc --prune=now
